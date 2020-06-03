c6c8964174e4674c5296584a69cc017c80a21429
Wills <jarquinwills@gmail.com>
2020-06-03 17:09:12 +0000

    The basic layout of the app has been stablished, but still needs some last touches and functionality.

---
+++ data.js
@@ -0,0 +1,35 @@
+var flashcardData = [
+  {
+    front: {
+      question: "What is Wills' middle name?",
+      hint: "Artist with last name Iglesias shares the same middle name",
+    },
+    back: {
+      answer: "Enrique",
+      description:
+        "Wills is named after his father(first name) and grandfather(middle name)",
+    },
+  },
+
+  {
+    front: {
+      question: "Where did Wills go to high school at?",
+      hint: "There is a movie about this high school",
+    },
+    back: {
+      answer: "T.C. Williams High School",
+      description: "Remember The Titans was based on this school",
+    },
+  },
+
+  {
+    front: {
+      question: "What is Wills' career in college?",
+      hint: "Creating this app is completely related to what he studies",
+    },
+    back: {
+      answer: "Applied Computer Science: Software Engineering",
+      description: "He specializes on web development",
+    },
+  },
+];
